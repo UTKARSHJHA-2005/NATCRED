08bb02274fa828ec2160dc53941451bd440d61d5
UTKARSH JHA <147072811+UTKARSHJHA-2005@users.noreply.github.com>
2025-10-09 12:34:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ['http://localhost:5173', 'https://natcred.vercel.app'],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
