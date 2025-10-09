9ca5ba48c70c28acbc577b827e515bc365cd1a1d
UTKARSH JHA <147072811+UTKARSHJHA-2005@users.noreply.github.com>
2025-10-09 11:05:24 +0530

    Update Posts.jsx

--- client/src/pages/Posts.jsx
+++ client/src/pages/Posts.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 import PostsPage from '../components/PostPage'
 
 export default function Posts() {
