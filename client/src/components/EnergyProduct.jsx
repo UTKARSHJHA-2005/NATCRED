99bcec34203af5e86f6a94218ce98e344035d03f
UTKARSH JHA <147072811+UTKARSHJHA-2005@users.noreply.github.com>
2025-10-09 14:00:34 +0530

    Update EnergyProduct.jsx

--- client/src/components/EnergyProduct.jsx
+++ client/src/components/EnergyProduct.jsx
@@ -65,12 +65,14 @@ export default function EnergyProduct() {
         </Swiper>
         {/* View All Button */}
         <div data-aos='slip-left' className="text-center mt-8">
+         <Link to="/projects">
           <button className="px-4 py-2 bg-blue-600 border-green-400 border-[2px] text-white rounded-lg hover:bg-black" style={{
             boxShadow: "0px 0px 20px rgba(0, 255, 136, 0.4)"
           }}>
             View All Products
           </button>
-        </div>
+        </Link>
+       </div>
       </div>
     </section>
   );
